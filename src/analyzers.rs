1891c42254159528ebad5f00dbe8bbec1654d2e4
SiriusAshling <53594055+SiriusAshling@users.noreply.github.com>
2021-07-19 14:53:49 +0200

    shop cost totals

--- src/analyzers.rs
+++ src/analyzers.rs
@@ -1,4 +1,7 @@
-use seedgen::util::settings;
+use seedgen::{
+    util::settings,
+    util::constants::SHOP_PRICES,
+};
 
 pub trait Analyzer {
     fn name(&self) -> String;
@@ -42,5 +45,41 @@ impl Analyzer for LaunchTiming {
         }
 
         panic!("No Launch in seed!");
+    }
+}
+
+pub struct ShopCostTotal {}
+impl Analyzer for ShopCostTotal {
+    fn name(&self) -> String { String::from("Shop Cost Total") }
+    fn value(&self, seed: &str) -> String {
+        let mut shop_states = SHOP_PRICES.iter().map(|(_, _, state)| format!("|8|{}|int|", state)).collect::<Vec<_>>();
+        let mut total_price = 0;
+
+        'outer: for line in seed.lines() {
+            for (state_index, shop_state) in shop_states.iter().enumerate() {
+                if let Some(start_index) = line.find(shop_state) {
+                    let untrimmed = &line[start_index + shop_state.len()..];
+                    let end_index = untrimmed.find(char::is_whitespace).unwrap_or_else(|| untrimmed.len());
+                    let trimmed = &untrimmed[..end_index];
+
+                    let price: u16 = trimmed.parse().unwrap_or_else(|_| panic!("Failed to parse price \"{}\" from \"{}\"", trimmed, line));
+                    total_price += price;
+
+                    shop_states.remove(state_index);
+
+                    if shop_states.is_empty() {
+                        break 'outer;
+                    }
+                    break;
+                }
+
+            }
+        }
+
+        for shop_state in shop_states {
+            eprintln!("Warn: couldn't find price for shop item {}", shop_state);
+        }
+
+        return total_price.to_string();
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -7,20 +7,19 @@ use std::{
 
 use seedgen::{
     lexer,
-    util::{
-        settings::{Spawn, Settings},
-    },
+    util::settings::Settings,
 };
 
 use analyzers::Analyzer;
 
-const SAMPLE_SIZE: usize = 100;
+const SAMPLE_SIZE: usize = 500;
 
 fn main() {
     let analyzers: Vec<Box<dyn Analyzer>> = vec![
         Box::new(analyzers::SpawnLocation {}),
         Box::new(analyzers::LaunchLocation {}),
         Box::new(analyzers::LaunchTiming {}),
+        Box::new(analyzers::ShopCostTotal {}),
     ];
 
     let mut out = String::new();
@@ -33,7 +32,9 @@ fn main() {
     out.push('\n');
 
     let mut settings = Settings::default();
-    settings.spawn_loc = Spawn::Random;
+    settings.presets.push(PathBuf::from("gorlek"));
+    settings = settings.apply_presets().unwrap();
+    let headers = vec![];
     let graph = lexer::parse_logic(&PathBuf::from("areas.wotw"), &PathBuf::from("loc_data.csv"), &PathBuf::from("state_data.csv"), &settings.pathsets, false).unwrap();
 
     for index in 0..SAMPLE_SIZE {
@@ -41,7 +42,7 @@ fn main() {
         eprint!("{}", progress);
 
         let mut line = Vec::new();
-        let seed = seedgen::generate_seed(&graph, &settings, &vec![], None).unwrap().pop().unwrap();
+        let seed = seedgen::generate_seed(&graph, settings.clone(), &headers, None).unwrap().0.pop().unwrap();
 
         for analyzer in &analyzers {
             line.push(analyzer.value(&seed));
